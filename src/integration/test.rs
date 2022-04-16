b04857d77505683fa0f2bc39e014b2a705feb487
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-04-16 02:54:11 -0400

    ✅ Improves tests.

--- src/integration/test.rs
+++ src/integration/test.rs
@@ -1,11 +1,17 @@
+use std::net::{IpAddr, Ipv4Addr};
+
 use error::Error;
+use eui48::MacAddress;
+use serde_json::Map;
+use time::{OffsetDateTime, PrimitiveDateTime};
+use uuid::Uuid;
 
 use crate::integration::cornucopia_gen::{
     queries::module_2::{books_opt_ret_param, BooksOptRetParam},
     types::public::SpongebobCharacter,
 };
 
-use super::cornucopia_gen;
+use super::cornucopia_gen::{self, queries::module_2::select_everything};
 use deadpool_postgres::Client;
 
 async fn setup() -> Result<Client, crate::error::Error> {
@@ -15,7 +21,7 @@ async fn setup() -> Result<Client, crate::error::Error> {
     container::setup()?;
     let pool = cli_pool()?;
     let client = pool.get().await?;
-    run_migrations(&client, "tests/migrations").await?;
+    run_migrations(&client, "src/integration/migrations").await?;
 
     Ok(client)
 }
@@ -35,6 +41,11 @@ async fn integration() -> Result<(), Error> {
     author_name_by_id_test(&client).await?;
     author_name_by_id_opt_test(&client).await?;
     author_name_starting_with_test(&client).await?;
+    return_custom_type_test(&client).await?;
+    select_where_custom_type_test(&client).await?;
+    select_everything_test(&client).await?;
+
+    // ! This test must be last because it has side-effects
     insert_books_test(&client).await?;
 
     teardown().await?;
@@ -46,8 +57,8 @@ async fn integration() -> Result<(), Error> {
 
 async fn integration_test() {
     if let Err(e) = integration().await {
-        println!("{}", e);
         let _ = teardown().await;
+        panic!("{}", e)
     }
 }
 
@@ -263,6 +274,103 @@ async fn select_where_custom_type_test(client: &Client) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+async fn select_everything_test(client: &Client) -> Result<(), Error> {
+    //'1999-01-08', '04:05:06.789', '{}', '{}', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '127.0.0.1', '08:00:2b:01:02:03'
+
+    let primitive_datetime_format =
+        time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
+    let primitive_datetime =
+        PrimitiveDateTime::parse("2020-01-02 03:04:05", &primitive_datetime_format).unwrap();
+    let offset_datetime = OffsetDateTime::parse(
+        "1985-04-12T23:20:50.52Z",
+        &time::format_description::well_known::Rfc3339,
+    )
+    .unwrap();
+
+    let expected = (
+        true,
+        true,
+        42i8,
+        300i16,
+        300i16,
+        300i16,
+        300i16,
+        100000i32,
+        100000i32,
+        100000i32,
+        100000i32,
+        10000000000i64,
+        10000000000i64,
+        10000000000i64,
+        10000000000i64,
+        1.12f32,
+        1.12f32,
+        1.1231231231f64,
+        1.1231231231f64,
+        String::from("hello"),
+        String::from("hello"),
+        vec![222u8, 173u8, 190u8, 239u8],
+        primitive_datetime.clone(),
+        primitive_datetime,
+        offset_datetime.clone(),
+        offset_datetime,
+        time::Date::from_calendar_date(1999, time::Month::January, 8).unwrap(),
+        time::Time::from_hms_milli(4, 5, 6, 789).unwrap(),
+        serde_json::Value::Object(Map::new()),
+        serde_json::Value::Object(Map::new()),
+        Uuid::parse_str("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11").unwrap(),
+        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+        MacAddress::new([8, 0, 43, 1, 2, 3]),
+    );
+    let actual = select_everything(client).await?;
+
+    assert_eq(expected.0, actual.0)?;
+    assert_eq(expected.1, actual.1)?;
+    assert_eq(expected.2, actual.2)?;
+    assert_eq(expected.3, actual.3)?;
+    assert_eq(expected.4, actual.4)?;
+    assert_eq(expected.5, actual.5)?;
+    assert_eq(expected.6, actual.6)?;
+    assert_eq(expected.7, actual.7)?;
+    assert_eq(expected.8, actual.8)?;
+    assert_eq(expected.9, actual.9)?;
+    assert_eq(expected.10, actual.10)?;
+    assert_eq(expected.11, actual.11)?;
+    assert_eq(expected.12, actual.12)?;
+    assert_eq(expected.13, actual.13)?;
+    assert_eq(expected.14, actual.14)?;
+    assert_eq(expected.15, actual.15)?;
+    assert_eq(expected.16, actual.16)?;
+    assert_eq(expected.17, actual.17)?;
+    assert_eq(expected.18, actual.18)?;
+    assert_eq(expected.19, actual.19)?;
+    assert_eq(expected.20, actual.20)?;
+    assert_eq(expected.21, actual.21)?;
+    assert_eq(expected.22, actual.22)?;
+    assert_eq(expected.23, actual.23)?;
+    assert_eq(expected.24, actual.24)?;
+    assert_eq(expected.25, actual.25)?;
+    assert_eq(expected.26, actual.26)?;
+    assert_eq(expected.27, actual.27)?;
+    assert_eq(expected.28, actual.28)?;
+    assert_eq(expected.29, actual.29)?;
+    assert_eq(expected.30, actual.30)?;
+    assert_eq(expected.31, actual.31)?;
+
+    Ok(())
+}
+
+fn assert_eq<T: std::fmt::Debug + PartialEq>(expected: T, actual: T) -> Result<(), Error> {
+    if actual != expected {
+        Err(Error::Integration {
+            expected: format!("{:?}", expected),
+            actual: format!("{:?}", actual),
+        })
+    } else {
+        Ok(())
+    }
 }
 
 mod error {
