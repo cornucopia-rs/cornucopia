fd779bad0ee9a9976a0edac07a57cb2c4fae729d
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-05-19 20:14:04 -0400

    🚧 Temporarily disable tests.

--- src/integration.rs
+++ src/integration.rs
@@ -3,8 +3,8 @@
 #[allow(unused_imports)]
 #[allow(dead_code)]
 mod cornucopia;
-#[allow(clippy::all)]
-#[allow(unused_variables)]
-#[allow(unused_imports)]
-#[allow(dead_code)]
-mod test;
+// #[allow(clippy::all)]
+// #[allow(unused_variables)]
+// #[allow(unused_imports)]
+// #[allow(dead_code)]
+// mod test;
