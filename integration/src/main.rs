096b94308720a8dfdc22fd63b0736a5e3e9ab86e
Virgiel <>
2022-05-22 17:49:49 +0200

    Run test as cli and as cargo test

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -1,13 +1,13 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, process::ExitCode};
 
 use clap::Parser;
 use owo_colors::OwoColorize;
 
-/// Run cornucopia test runner
+/// Start cornucopia test runner
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Args {
-    /// Apply returned error to the test description if not matching
+    /// Format test descriptors and update error msg
     #[clap(short, long)]
     apply: bool,
 }
@@ -28,21 +28,32 @@ pub struct Test<'a> {
     error: Cow<'a, str>,
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
+    if run_test(args.apply) {
+        ExitCode::SUCCESS
+    } else {
+        ExitCode::FAILURE
+    }
+}
 
-    let got_msg = if args.apply {
+// Run test, return true if all test are successful
+fn run_test(apply: bool) -> bool {
+    let mut successful = true;
+
+    let got_msg = if apply {
         "Apply:".bright_black()
     } else {
         "Got:".bright_black()
     };
-    let expected_msg = if args.apply {
+    let expected_msg = if apply {
         "Previous:".bright_black()
     } else {
         "Expected:".bright_black()
     };
 
-    // TODO use cornucopia lib API
+    // TODO reuse same connection using cornucopia lib API
+
     let original_pwd = std::env::current_dir().unwrap();
     for file in std::fs::read_dir("fixtures").unwrap() {
         let file = file.unwrap();
@@ -55,37 +66,32 @@ fn main() {
             // Generate file tree path
             let temp_dir = tempfile::tempdir().unwrap();
 
+            // We need to change current dir for error path to always be the same
             std::env::set_current_dir(&temp_dir).unwrap();
-            let queries_path = "queries";
-            let migrations_path = "migrations";
-            let out_path = "cornucopia.rs";
 
-            // Generate file tree content
-            std::fs::create_dir(&queries_path).unwrap();
-            std::fs::create_dir(&migrations_path).unwrap();
-
+            // Generate migrations files
+            std::fs::create_dir("migrations").unwrap();
             if let Some(migration) = test.migration {
                 let name = test.migration_name.unwrap_or("1653210840_first.sql");
                 std::fs::write(&format!("migrations/{name}"), migration).unwrap();
             }
+
+            // generate queries files
+            std::fs::create_dir("queries").unwrap();
             if let Some(query) = test.query {
                 let name = test.query_name.unwrap_or("module_1.sql");
                 std::fs::write(&format!("queries/{name}"), query).unwrap();
             }
 
+            // Run command
             let output = std::process::Command::new("cornucopia")
                 .arg("generate")
-                .arg("-q")
-                .arg(queries_path)
-                .arg("-m")
-                .arg(migrations_path)
-                .arg("-d")
-                .arg(out_path)
                 .output()
                 .unwrap();
 
             let err = String::from_utf8(output.stderr).unwrap();
             if err.trim() != test.error.trim() {
+                successful = false;
                 println!(
                     "{} {}\n{}\n{}\n{}\n{}",
                     test.name,
@@ -98,14 +104,27 @@ fn main() {
             } else {
                 println!("{} {}", test.name, "OK".green());
             }
-            if args.apply {
+            if apply {
                 test.error = Cow::Owned(err.trim().to_string())
             }
             std::env::set_current_dir(&original_pwd).unwrap();
         }
-        if args.apply {
+
+        if apply {
+            // Format test descriptor and update error message if needed
             let edited = toml::to_string_pretty(&suite).unwrap();
             std::fs::write(file.path(), edited).unwrap()
         }
+    }
+    return successful;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::run_test;
+
+    #[test]
+    fn run() {
+        assert!(run_test(false))
     }
 }
