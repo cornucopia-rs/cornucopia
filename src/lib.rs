af4bd910b423fd78808ea4b015f2743600fa7339
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-05-24 23:03:41 -0400

    ✨ Update lib API to use sync `postgres`.

--- src/lib.rs
+++ src/lib.rs
@@ -13,26 +13,29 @@ pub(crate) mod type_registrar;
 
 use codegen::generate as generate_internal;
 use error::{NewMigrationError, WriteCodeGenFileError};
+use postgres::Client;
 use prepare_queries::prepare;
 use read_queries::read_query_modules;
 use run_migrations::run_migrations as run_migrations_internal;
 use std::path::Path;
 use time::OffsetDateTime;
-use tokio_postgres::Client;
 use type_registrar::TypeRegistrar;
 
 pub use cli::run;
 pub use error::Error;
 
 /// Runs the migrations given at `migrations_path` at the specified `url`.
-pub async fn run_migrations(client: &Client, migrations_path: &str) -> Result<(), Error> {
-    Ok(crate::run_migrations::run_migrations(client, migrations_path).await?)
+pub fn run_migrations(client: &mut Client, migrations_path: &str) -> Result<(), Error> {
+    Ok(crate::run_migrations::run_migrations(
+        client,
+        migrations_path,
+    )?)
 }
 
 /// Creates a new migration file at the specified `migrations_path`.
 /// The full name of the migration will correspond to `timestamp`_`name`.sql
 /// where `timestamp is the unix time when the migration was created.`
-pub async fn new_migration(migrations_path: &str, name: &str) -> Result<(), Error> {
+pub fn new_migration(migrations_path: &str, name: &str) -> Result<(), Error> {
     // Create a timestamp of the current time.
     let unix_ts = OffsetDateTime::now_utc().unix_timestamp();
     // Format the target file name
@@ -50,16 +53,17 @@ pub async fn new_migration(migrations_path: &str, name: &str) -> Result<(), Erro
 
 /// Generates your cornucopia queries residing in `queries_path` against the database
 /// at `url`. If some `destination` is given, the generated code will be written at that path.
-pub async fn generate_live(
-    client: &Client,
+pub fn generate_live(
+    client: &mut Client,
     queries_path: &str,
     destination: Option<&str>,
+    is_async: bool,
 ) -> Result<String, Error> {
     let mut type_registrar = TypeRegistrar::default();
 
     let modules = read_query_modules(queries_path)?;
-    let prepared_modules = prepare(client, &mut type_registrar, modules).await?;
-    let generated_code = generate_internal(&type_registrar, prepared_modules)?;
+    let prepared_modules = prepare(client, &mut type_registrar, modules)?;
+    let generated_code = generate_internal(&type_registrar, prepared_modules, is_async)?;
 
     if let Some(d) = destination {
         write_generated_code(d, &generated_code)?
@@ -72,20 +76,21 @@ pub async fn generate_live(
 /// managed by cornucopia. The database is created using the migrations in the given
 /// `migrations_path` folder.
 /// If some `destination` is given, the generated code will be written at that path.
-pub async fn generate_managed(
-    client: &Client,
+pub fn generate_managed(
+    client: &mut Client,
     queries_path: &str,
     migrations_path: &str,
     destination: Option<&str>,
     podman: bool,
+    is_async: bool,
 ) -> Result<String, Error> {
     let mut type_registrar = TypeRegistrar::default();
 
     let modules = read_query_modules(queries_path)?;
     container::setup(podman)?;
-    run_migrations_internal(client, migrations_path).await?;
-    let prepared_modules = prepare(client, &mut type_registrar, modules).await?;
-    let generated_code = generate_internal(&type_registrar, prepared_modules)?;
+    run_migrations_internal(client, migrations_path)?;
+    let prepared_modules = prepare(client, &mut type_registrar, modules)?;
+    let generated_code = generate_internal(&type_registrar, prepared_modules, is_async)?;
     container::cleanup(podman)?;
 
     if let Some(destination) = destination {
