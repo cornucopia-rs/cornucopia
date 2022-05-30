c6e65d3e41598cca6339ee9be805060efbd561b5
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-05-30 13:40:34 -0400

    ✨  Update grammar.

--- src/parser.rs
+++ src/parser.rs
@@ -28,7 +28,9 @@ pub(crate) struct Parsed<T> {
 pub(crate) struct ParsedQuery {
     pub(crate) line: usize,
     pub(crate) name: String,
+    pub(crate) named_param_struct: Option<Parsed<String>>,
     pub(crate) params: Vec<Parsed<String>>,
+    pub(crate) named_return_struct: Option<Parsed<String>>,
     pub(crate) nullable_columns: Vec<Parsed<NullableColumn>>,
     pub(crate) sql_str: String,
 }
@@ -42,117 +44,131 @@ pub(crate) fn parse_queries(input: &str) -> Result<Vec<ParsedQuery>, Error> {
         .unwrap()
         .into_inner();
 
-    parser_tokens
-        .filter(|r| matches!(r.as_rule(), Rule::query))
-        .map(|query| {
-            let (line, _) = query.as_span().start_pos().line_col();
-            let mut query_tokens = query.into_inner();
+    let mut parsed_queries = Vec::new();
 
-            // Parse annotation
-            let mut annotation_tokens = query_tokens.next().unwrap().into_inner();
+    for query in parser_tokens.filter(|r| matches!(r.as_rule(), Rule::query)) {
+        let (line, _) = query.as_span().start_pos().line_col();
+        let mut query_tokens = query.into_inner();
 
-            // Parse sql
-            let sql_tokens = query_tokens.next().unwrap();
-            let sql_str = sql_tokens.as_str().to_string();
-            let sql_start = sql_tokens.as_span().start();
+        // Parse annotation
+        let mut annotation_tokens = query_tokens.next().unwrap().into_inner();
 
-            // Name
-            let name_tokens = annotation_tokens.next().unwrap();
-            let name = name_tokens.as_str().to_string();
+        // Parse sql
+        let sql_tokens = query_tokens.next().unwrap();
+        let sql_str = sql_tokens.as_str().to_string();
+        let sql_start = sql_tokens.as_span().start();
 
-            // Parameter list and nullable column list
-            Ok(match annotation_tokens.next() {
-                Some(it) => match it.as_rule() {
-                    // Postgres-compatible syntax
-                    Rule::param_list => {
-                        // Params
-                        let params = parse_params(it)?;
-                        let nb_params = params.len();
+        // Name
+        let name_tokens = annotation_tokens.next().unwrap();
+        let name = name_tokens.as_str().to_string();
 
-                        // Nullable columns
-                        let nullable_columns = match annotation_tokens.next() {
-                            Some(it_nullable_columns) => {
-                                parse_nullable_columns(it_nullable_columns)?
-                            }
-                            None => Vec::new(),
-                        };
+        let mut parsed_query = ParsedQuery {
+            line,
+            name,
+            params: Vec::new(),
+            nullable_columns: Vec::new(),
+            sql_str: sql_str.clone(),
+            named_param_struct: None,
+            named_return_struct: None,
+        };
 
-                        // Bind params
-                        let bind_params = parse_pg_bind_params(sql_tokens)?;
+        // Parameter list and nullable column list
+        match annotation_tokens.next() {
+            Some(it) => match it.as_rule() {
+                Rule::param_list => {
+                    // Params
+                    let params = parse_params(it)?;
+                    let nb_params = params.len();
 
-                        // Check if the bind parameter's index is greater than the number of parameters
-                        if let Some(p) = bind_params.iter().find(|p| p.value > nb_params as i16) {
-                            return Err(Error::Validation(
-                                ValidationError::MoreBindParamsThanParams {
-                                    nb_params,
-                                    pos: ErrorPosition {
-                                        line: p.line,
-                                        col: p.col,
-                                        line_str: p.line_str.to_owned(),
-                                    },
-                                },
-                            ));
-                        };
-
-                        // Check that every param is used in the query
-                        if let Some((index, p)) = params.iter().enumerate().find(|(index, _)| {
-                            !bind_params
-                                .iter()
-                                .any(|bind_index| bind_index.value == *index as i16 + 1)
-                        }) {
-                            return Err(Error::Validation(ValidationError::UnusedParam {
-                                index: index + 1,
-                                pos: ErrorPosition {
-                                    line: p.line,
-                                    col: p.col,
-                                    line_str: p.line_str.to_owned(),
-                                },
-                            }));
-                        }
-
-                        ParsedQuery {
-                            line,
-                            name,
-                            params,
-                            nullable_columns,
-                            sql_str,
-                        }
-                    }
-                    // Extended syntax with nullabble columns
-                    Rule::nullable_column_list => {
-                        // Nullable columns
-                        let nullable_columns = parse_nullable_columns(it)?;
-                        // Bind params and normalized sql
-                        let (bind_params, sql_str) =
-                            parse_extended_bind_params(sql_tokens, sql_start, &sql_str)?;
-                        ParsedQuery {
-                            line,
-                            name,
-                            params: bind_params,
-                            nullable_columns,
-                            sql_str,
-                        }
-                    }
-                    _ => unreachable!(),
-                },
-                // Extended Syntax without nullable columns
-                None => {
                     // Nullable columns
-                    let nullable_columns = Vec::new();
-                    // Bind params and normalized sql
-                    let (bind_params, sql_str) =
-                        parse_extended_bind_params(sql_tokens, sql_start, &sql_str)?;
-                    ParsedQuery {
-                        line,
-                        name,
-                        params: bind_params,
-                        nullable_columns,
-                        sql_str,
+                    if let Some(nullable_col_tokens) = annotation_tokens.next() {
+                        parsed_query.nullable_columns = parse_nullable_columns(nullable_col_tokens)?
+                    };
+
+                    // Bind params
+                    if params.len() == 1 {
+                        match parse_extended_bind_params(sql_tokens.clone(), sql_start, &sql_str) {
+                            Ok((bind_params, normalized_sql)) => {
+                                // Nullable columns
+                                if let Some(return_struct_name) = annotation_tokens.next() {
+                                    let pos = return_struct_name.as_span().start_pos();
+                                    let (line, col) = pos.line_col();
+                                    let line_str = pos.line_of().to_owned();
+                                    parsed_query.named_return_struct = Some(Parsed {
+                                        line,
+                                        col,
+                                        line_str,
+                                        value: return_struct_name.as_str().to_string(),
+                                    });
+                                };
+                                parsed_query.params = bind_params;
+                                parsed_query.sql_str = normalized_sql;
+                            }
+                            Err(_) => {
+                                let bind_params = parse_pg_bind_params(sql_tokens)?;
+                                // Check if the bind parameter's index is greater than the number of parameters
+                                Error::validate_more_bind_params_than_params(
+                                    &bind_params,
+                                    nb_params,
+                                )?;
+                                // Check that every param is used in the query
+                                Error::validate_unused_param(&params, &bind_params)?;
+                            }
+                        }
+                    } else {
+                        let bind_params = parse_pg_bind_params(sql_tokens)?;
+                        // Check if the bind parameter's index is greater than the number of parameters
+                        Error::validate_more_bind_params_than_params(&bind_params, nb_params)?;
+                        // Check that every param is used in the query
+                        Error::validate_unused_param(&params, &bind_params)?;
                     }
                 }
-            })
-        })
-        .collect()
+                // Extended syntax with nullabble columns
+                Rule::nullable_column_list => {
+                    // Nullable columns
+                    let nullable_columns = parse_nullable_columns(it)?;
+                    // Bind params and normalized sql
+                    let (bind_params, normalized_sql) =
+                        parse_extended_bind_params(sql_tokens, sql_start, &sql_str)?;
+                    parsed_query.nullable_columns = nullable_columns;
+                    parsed_query.params = bind_params;
+                    parsed_query.sql_str = normalized_sql;
+                }
+                Rule::ident => {
+                    let pos = it.as_span().start_pos();
+                    let (line, col) = pos.line_col();
+                    let line_str = pos.line_of().to_owned();
+                    parsed_query.named_return_struct = Some(Parsed {
+                        line,
+                        col,
+                        line_str,
+                        value: it.as_str().to_string(),
+                    });
+                    // Nullable columns
+                    if let Some(nullable_col_tokens) = annotation_tokens.next() {
+                        parsed_query.nullable_columns = parse_nullable_columns(nullable_col_tokens)?
+                    };
+                    // Bind params and normalized sql
+                    let (bind_params, normalized_sql) =
+                        parse_extended_bind_params(sql_tokens, sql_start, &sql_str)?;
+                    parsed_query.params = bind_params;
+                    parsed_query.sql_str = normalized_sql;
+                }
+                _ => unreachable!(),
+            },
+            // Extended Syntax without nullable columns
+            None => {
+                // Bind params and normalized sql
+                let (bind_params, normalized_sql) =
+                    parse_extended_bind_params(sql_tokens, sql_start, &sql_str)?;
+                parsed_query.params = bind_params;
+                parsed_query.sql_str = normalized_sql;
+            }
+        };
+        parsed_queries.push(parsed_query);
+    }
+
+    Ok(parsed_queries)
 }
 
 /// Parse query parameters. This is only applicable to postgres-compatible queries.
@@ -162,26 +178,15 @@ fn parse_params(pair: Pair<Rule>) -> Result<Vec<Parsed<String>>, Error> {
         if it.as_rule() == Rule::ident {
             // Collect info about the span we're parsing
             let it_str = it.as_str().to_owned();
-            let pos = it.as_span().start_pos();
-            let (line, col) = pos.line_col();
-            let line_str = pos.line_of().to_owned();
+
             // Check that this parameter is not already in the list, then add it to the list.
-            if params.iter().any(|p: &Parsed<String>| p.value == it_str) {
-                return Err(Error::Validation(ValidationError::DuplicateParam {
-                    pos: ErrorPosition {
-                        line,
-                        col,
-                        line_str,
-                    },
-                }));
-            } else {
-                params.push(Parsed {
-                    value: it_str,
-                    line,
-                    col,
-                    line_str,
-                });
-            }
+            let (line, col, line_str) = Error::validate_duplicate_param(it, &params, &it_str)?;
+            params.push(Parsed {
+                value: it_str,
+                line,
+                col,
+                line_str,
+            });
         }
     }
     Ok(params)
@@ -201,26 +206,7 @@ fn parse_pg_bind_params(pair: Pair<Rule>) -> Result<Vec<Parsed<i16>>, Error> {
             let it_str = it.as_str().to_owned();
 
             // Check that the index can be parsed as a i16 (required by postgres wire protocol)
-            let index = it_str.parse::<i16>().map_err(|_| {
-                Error::Validation(ValidationError::InvalidI16Index {
-                    pos: ErrorPosition {
-                        line,
-                        col,
-                        line_str: line_str.to_owned(),
-                    },
-                })
-            })?;
-
-            // Check that the index is also non-zero (postgres bind params are 1-indexed)
-            if index == 0 {
-                return Err(Error::Validation(ValidationError::InvalidI16Index {
-                    pos: ErrorPosition {
-                        line,
-                        col,
-                        line_str,
-                    },
-                }));
-            };
+            let index = Error::validate_invalid_i16_index(&it_str, line, col, &line_str)?;
 
             // If the bind param has not yet been seen, add it to the list
             if !bind_params.iter().any(|p: &Parsed<i16>| p.value == index) {
@@ -330,25 +316,7 @@ fn parse_nullable_columns(pair: Pair<Rule>) -> Result<Vec<Parsed<NullableColumn>
             // Indexed nullable column
             Rule::number => {
                 // Check that the index can be parsed as a i16 (required by postgres wire protocol)
-                let index = it_str.parse::<i16>().map_err(|_| {
-                    Error::Validation(ValidationError::InvalidI16Index {
-                        pos: ErrorPosition {
-                            line,
-                            col,
-                            line_str: line_str.to_owned(),
-                        },
-                    })
-                })?;
-                // Check that index is not zero (Postgres columns are 1-indexed)
-                if index == 0 {
-                    return Err(Error::Validation(ValidationError::InvalidI16Index {
-                        pos: ErrorPosition {
-                            line,
-                            col,
-                            line_str,
-                        },
-                    }));
-                }
+                let index = Error::validate_invalid_i16_index(it_str, line, col, &line_str)?;
                 NullableColumn::Index(index)
             }
             _ => unreachable!(),
@@ -365,13 +333,111 @@ fn parse_nullable_columns(pair: Pair<Rule>) -> Result<Vec<Parsed<NullableColumn>
 }
 
 pub(crate) mod error {
-    use super::Rule;
+    use pest::iterators::Pair;
+
+    use super::{Parsed, Rule};
     use std::fmt::Display;
 
     #[derive(Debug)]
     pub(crate) enum Error {
         Parser(pest::error::Error<Rule>),
         Validation(ValidationError),
+    }
+
+    impl Error {
+        pub(crate) fn validate_more_bind_params_than_params(
+            bind_params: &[Parsed<i16>],
+            nb_params: usize,
+        ) -> Result<(), Self> {
+            if let Some(p) = bind_params.iter().find(|p| p.value > nb_params as i16) {
+                return Err(Error::Validation(
+                    ValidationError::MoreBindParamsThanParams {
+                        nb_params,
+                        pos: ErrorPosition {
+                            line: p.line,
+                            col: p.col,
+                            line_str: p.line_str.to_owned(),
+                        },
+                    },
+                ));
+            } else {
+                Ok(())
+            }
+        }
+
+        pub(crate) fn validate_unused_param(
+            params: &[Parsed<String>],
+            bind_params: &[Parsed<i16>],
+        ) -> Result<(), Self> {
+            if let Some((index, p)) = params.iter().enumerate().find(|(index, _)| {
+                !bind_params
+                    .iter()
+                    .any(|bind_index| bind_index.value == *index as i16 + 1)
+            }) {
+                return Err(Error::Validation(ValidationError::UnusedParam {
+                    index: index + 1,
+                    pos: ErrorPosition {
+                        line: p.line,
+                        col: p.col,
+                        line_str: p.line_str.to_owned(),
+                    },
+                }));
+            } else {
+                Ok(())
+            }
+        }
+
+        pub(crate) fn validate_duplicate_param(
+            it: Pair<Rule>,
+            params: &[Parsed<String>],
+            param: &str,
+        ) -> Result<(usize, usize, String), Self> {
+            let pos = it.as_span().start_pos();
+            let (line, col) = pos.line_col();
+            let line_str = pos.line_of().to_owned();
+            if params.iter().any(|p: &Parsed<String>| p.value == param) {
+                return Err(Error::Validation(ValidationError::DuplicateParam {
+                    pos: ErrorPosition {
+                        line,
+                        col,
+                        line_str,
+                    },
+                }));
+            } else {
+                Ok((line, col, line_str))
+            }
+        }
+
+        pub(crate) fn validate_invalid_i16_index(
+            it_str: &str,
+            line: usize,
+            col: usize,
+            line_str: &str,
+        ) -> Result<i16, Error> {
+            // Check that the index can be parsed as a i16 (required by postgres wire protocol)
+            let index = it_str.parse::<i16>().map_err(|_| {
+                Error::Validation(ValidationError::InvalidI16Index {
+                    pos: ErrorPosition {
+                        line,
+                        col,
+                        line_str: line_str.to_owned(),
+                    },
+                })
+            })?;
+
+            // Check that the index is also non-zero (postgres bind params are 1-indexed)
+            if index == 0 {
+                return Err(Error::Validation(ValidationError::InvalidI16Index {
+                    pos: ErrorPosition {
+                        line,
+                        col,
+                        line_str: line_str.to_owned(),
+                    },
+                }));
+            };
+
+            Ok(index)
+        }
     }
 
     #[derive(Debug)]
