92320de2e90055870e2e8467dcae3c5dfac8e9a0
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-06-07 17:12:12 -0400

    ♻️ Decouple query reading from query parsing.

--- src/read_queries.rs
+++ src/read_queries.rs
@@ -1,26 +1,18 @@
-use crate::parser::{parse_query_module, ParsedQuery, ValidatedModule};
 use error::Error;
-
-#[derive(Debug)]
-pub(crate) struct Module {
-    pub(crate) path: String,
-    pub(crate) name: String,
-    pub(crate) inner: ValidatedModule,
-}
 
 /// Reads queries in the directory. Only .sql files are considered.
 ///
 /// # Error
 /// Returns an error if `dir_path` does not point to a valid directory or if a query file cannot be parsed.
-pub(crate) fn read_query_modules(dir_path: &str) -> Result<Vec<Module>, Error> {
-    let mut modules = Vec::new();
+pub(crate) fn read_query_modules(dir_path: &str) -> Result<Vec<(String, String, String)>, Error> {
+    let mut modules_info = Vec::new();
     for entry_result in std::fs::read_dir(dir_path).map_err(|err| Error {
-        err: err.into(),
+        err,
         path: String::from(dir_path),
     })? {
         // Directory entry
         let entry = entry_result.map_err(|err| Error {
-            err: err.into(),
+            err,
             path: dir_path.to_owned(),
         })?;
         let path_buf = entry.path();
@@ -39,43 +31,29 @@ pub(crate) fn read_query_modules(dir_path: &str) -> Result<Vec<Module>, Error> {
                 .to_string();
 
             let file_contents = std::fs::read_to_string(&path_buf).map_err(|err| Error {
-                err: err.into(),
+                err,
                 path: dir_path.to_owned(),
             })?;
 
-            let module = Module {
-                path: String::from(path_buf.to_string_lossy()),
-                name: module_name,
-                inner: parse_query_module(&file_contents).map_err(|err| Error {
-                    err: err.into(),
-                    path: String::from(path_buf.to_string_lossy()),
-                })?,
-            };
-
-            modules.push(module);
+            modules_info.push((
+                String::from(path_buf.to_string_lossy()),
+                module_name,
+                file_contents,
+            ));
         } else {
             continue;
         }
     }
-    Ok(modules)
+    Ok(modules_info)
 }
 
 pub(crate) mod error {
-    use crate::parser::error::Error as ParseError;
-
     use thiserror::Error as ThisError;
-
-    #[derive(Debug, ThisError)]
-    #[error("{0}")]
-    pub(crate) enum ErrorVariants {
-        Io(#[from] std::io::Error),
-        Parser(#[from] ParseError),
-    }
 
     #[derive(Debug, ThisError)]
     #[error("Error while reading queries [path: \"{path}\"]: {err}.")]
     pub struct Error {
-        pub(crate) err: ErrorVariants,
+        pub(crate) err: std::io::Error,
         pub(crate) path: String,
     }
 }
