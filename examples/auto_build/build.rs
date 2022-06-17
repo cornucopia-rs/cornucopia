1343fa0bcc02d8fa85220ae67b4b8a28ef4a58d2
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-06-16 21:46:33 -0400

    🩹 Comment out `auto_build` build script, with explanations.

--- examples/auto_build/build.rs
+++ examples/auto_build/build.rs
@@ -1,33 +1,31 @@
 use cornucopia::CodegenSettings;
 
 // This script will generate a new cornucopia file every time your migrations or queries change.
+// In this example, we generate the module in our project, but
+// we could also generate it elsewhere and embed the generated
+// file with a `include_str` statement in your project.
 fn main() -> Result<(), std::io::Error> {
-    // For the sake of simplicity, this example uses the defaults.
-    let queries_path = "queries";
-    let migrations_path = "migrations";
+    
+    //! To enable the automatic builds, remove the starting underscore 
+    //! from the variable names and uncomment the snippet below
+    let _queries_path = "queries";
+    let _migrations_path = "migrations";
+    let _destination = "src/cornucopia.rs";
+    let _settings = CodegenSettings {
+        is_async: true,
+        derive_ser: false,
+    };
 
-    // Again, for simplicity, we generate the module in our project, but
-    // we could've also generated it elsewhere if we wanted to.
-    // For example, you could make the destination the `target` folder
-    // and include the generated file with a `include_str` statement in your project.
-    let destination = "src/cornucopia.rs";
-
-    // Rerun this build script if the queries or migrations change.
-    println!("cargo:rerun-if-changed={queries_path}");
-    println!("cargo:rerun-if-changed={migrations_path}");
-
-    // Run cornucopia codegen
-    cornucopia::generate_managed(
-        queries_path,
-        migrations_path,
-        Some(destination),
-        false,
-        CodegenSettings {
-            is_async: true,
-            derive_ser: false,
-        },
-    )
-    .unwrap();
+    // println!("cargo:rerun-if-changed={queries_path}");
+    // println!("cargo:rerun-if-changed={migrations_path}");
+    // cornucopia::generate_managed(
+    //     queries_path,
+    //     migrations_path,
+    //     Some(destination),
+    //     false,
+    //     settings
+    // )
+    // .unwrap();
 
     Ok(())
 }
