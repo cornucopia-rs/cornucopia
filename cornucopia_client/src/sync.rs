5ac2cf7ae22778590ba273fa6a3b32b778fe7c4d
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-07-27 16:19:33 -0400

    🎨 Add `sync` feature.

---
+++ cornucopia_client/src/sync.rs
@@ -0,0 +1,33 @@
+use postgres::Statement;
+
+/// Cached statement
+pub struct Stmt {
+    query: &'static str,
+    cached: Option<Statement>,
+}
+
+impl Stmt {
+    #[must_use]
+    pub fn new(query: &'static str) -> Self {
+        Self {
+            query,
+            cached: None,
+        }
+    }
+
+    pub fn prepare<'a, C: postgres::GenericClient>(
+        &'a mut self,
+        client: &mut C,
+    ) -> Result<&'a Statement, postgres::Error> {
+        if self.cached.is_none() {
+            let stmt = client.prepare(self.query)?;
+            self.cached = Some(stmt);
+        }
+        // the statement is always prepared at this point
+        Ok(unsafe { self.cached.as_ref().unwrap_unchecked() })
+    }
+}
+
+pub trait Params<'a, S, O, C> {
+    fn bind(&'a self, client: &'a mut C, stmt: &'a mut S) -> O;
+}
