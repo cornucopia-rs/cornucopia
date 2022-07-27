4f2ad57f4cf54aae3e75c253d8be37b503e27066
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-07-27 16:20:49 -0400

    🎨 Hide more items behind `#[docs(hidden)]`.

--- cornucopia_client/src/lib.rs
+++ cornucopia_client/src/lib.rs
@@ -1,45 +1,21 @@
 mod array_iterator;
+
 #[cfg(feature = "async")]
+#[doc(hidden)]
 pub mod async_;
+
 #[cfg(feature = "deadpool")]
+#[doc(hidden)]
 mod deadpool;
+
+#[cfg(feature = "sync")]
+#[doc(hidden)]
+pub mod sync;
+
 #[doc(hidden)]
 pub mod private;
 
 pub use array_iterator::ArrayIterator;
 
-pub mod sync {
-    use postgres::Statement;
-
-    /// Cached statement
-    pub struct Stmt {
-        query: &'static str,
-        cached: Option<Statement>,
-    }
-
-    impl Stmt {
-        #[must_use]
-        pub fn new(query: &'static str) -> Self {
-            Self {
-                query,
-                cached: None,
-            }
-        }
-
-        pub fn prepare<'a, C: postgres::GenericClient>(
-            &'a mut self,
-            client: &mut C,
-        ) -> Result<&'a Statement, postgres::Error> {
-            if self.cached.is_none() {
-                let stmt = client.prepare(self.query)?;
-                self.cached = Some(stmt);
-            }
-            // the statement is always prepared at this point
-            Ok(unsafe { self.cached.as_ref().unwrap_unchecked() })
-        }
-    }
-
-    pub trait Params<'a, S, O, C> {
-        fn bind(&'a self, client: &'a mut C, stmt: &'a mut S) -> O;
-    }
-}
+#[cfg(feature = "async")]
+pub use async_::GenericClient;
