7455e834641d681c06a251bfae8bd5d797a081d6
louisgariepy <louis.gariepy.qc@hotmail.com>
2022-07-28 12:09:17 -0400

    🎨 Document and reexport `Params` trait for user discoverability.

--- cornucopia_client/src/async_.rs
+++ cornucopia_client/src/async_.rs
@@ -198,6 +198,8 @@ impl Stmt {
     }
 }
 
+/// This trait allows you to bind parameters to a query using a single
+/// struct, rather than passing each bind parameter as a function parameter.
 pub trait Params<'a, P, O, C> {
     fn params(&'a mut self, client: &'a C, params: &'a P) -> O;
 }
--- cornucopia_client/src/lib.rs
+++ cornucopia_client/src/lib.rs
@@ -1,5 +1,10 @@
 mod array_iterator;
 
+/// Hidden modules
+///
+/// The following modules are public because the generated code needs
+/// to access them, but users should not depend on them.
+
 #[cfg(feature = "async")]
 #[doc(hidden)]
 pub mod async_;
@@ -15,7 +20,19 @@ pub mod sync;
 #[doc(hidden)]
 pub mod private;
 
+/// Public items
+///
+/// These items are the real public part of the API.
 pub use array_iterator::ArrayIterator;
 
 #[cfg(feature = "async")]
 pub use async_::GenericClient;
+
+#[cfg(all(feature = "async", not(feature = "sync")))]
+pub use async_::Params;
+#[cfg(all(feature = "sync", feature = "async"))]
+pub use async_::Params as AsyncParams;
+#[cfg(all(feature = "sync", not(feature = "async")))]
+pub use sync::Params;
+#[cfg(all(feature = "sync", feature = "async"))]
+pub use sync::Params as SyncParams;
